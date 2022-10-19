326122d93c54cb5434950c77632208aa43444c90
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-19 18:20:20 +0300

    Scaffold Index Pages:

---
+++ partials/scripts.php
@@ -0,0 +1,18 @@
+<!--   Core JS Files   -->
+<script src="../public/js/core/popper.min.js"></script>
+<script src="../public/js/core/bootstrap.min.js"></script>
+<script src="../public/js/plugins/perfect-scrollbar.min.js"></script>
+<script src="../public/js/plugins/smooth-scrollbar.min.js"></script>
+<script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+        var options = {
+            damping: '0.5'
+        }
+        Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+</script>
+<!-- Github buttons -->
+<script async defer src="https://buttons.github.io/buttons.js"></script>
+<!-- Control Center for Material Dashboard: parallax effects, scripts for the example pages etc -->
+<script src="../public/js/material-dashboard.min.js?v=3.0.0"></script>
