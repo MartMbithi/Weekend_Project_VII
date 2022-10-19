134a9e036c4ed91b7a20e4fec6b3ba4dc87c3cb3
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-19 18:53:53 +0300

    Register Auth Server Side Intergrations

--- helpers/authentication.php
+++ helpers/authentication.php
@@ -2,35 +2,26 @@
 
 /* Login */
 if (isset($_POST['Login'])) {
-    $login_username = mysqli_real_escape_string($mysqli, $_POST['login_username']);
-    $login_password = md5(mysqli_real_escape_string($mysqli, $_POST['login_password']));
+    $admin_email = mysqli_real_escape_string($mysqli, $_POST['admin_email']);
+    $admin_password = md5(mysqli_real_escape_string($mysqli, $_POST['admin_password']));
 
     /* Process Login */
-    $stmt = $mysqli->prepare("SELECT login_username, login_password, login_rank, login_id FROM login 
-    WHERE login_username = '{$login_username}' AND login_password = '{$login_password}'");
+    $stmt = $mysqli->prepare("SELECT admin_email, admin_password, admin_id FROM administrator 
+    WHERE admin_email = '{$admin_email}' AND admin_password = '{$admin_password}'");
     $stmt->execute();
-    $stmt->bind_result($login_username, $login_password, $login_rank, $login_id);
+    $stmt->bind_result($admin_email, $admin_password, $admin_id);
     $rs = $stmt->fetch();
 
     /* Session Variables */
-    $_SESSION['login_id'] = $login_id;
-    $_SESSION['login_rank'] = $login_rank;
+    $_SESSION['admin_id'] = $admin_id;
 
-    if (($rs && $login_rank == "administrator")) {
+    if ($rs) {
         /* Pass This Alert Via Session */
-        $_SESSION['success'] = 'You Have Successfully Logged In';
+        $_SESSION['success'] = 'Login successful';
         header('Location: dashboard');
         exit;
-    } elseif ($rs && $user_access_level == "owner") {
-        $_SESSION['success'] = 'Successfully logged in as pet owner';
-        header('Location: owner_dashboard');
-        exit;
-    } elseif ($rs && $user_access_level == "adopter") {
-        $_SESSION['success'] = 'Successfully logged in as pet adopter';
-        header('Location: adopter_dashboard');
-        exit;
     } else {
-        $err = "Access Denied Please Check Your Email Or Password";
+        $err = "Incorrect email or password";
     }
 }
 
@@ -38,14 +29,14 @@ if (isset($_POST['Login'])) {
 
 /* Reset Password Step 1 */
 if (isset($_POST['Reset_Password_Step_1'])) {
-    $login_username = mysqli_real_escape_string($mysqli, $_POST['login_username']);
+    $admin_email = mysqli_real_escape_string($mysqli, $_POST['admin_email']);
     /* Check If User Exists */
-    $sql = "SELECT * FROM  login WHERE login_username = '{$login_username}'";
+    $sql = "SELECT * FROM  administrator WHERE admin_email = '{$admin_email}'";
     $res = mysqli_query($mysqli, $sql);
     if (mysqli_num_rows($res) > 0) {
         /* Redirect User To Confirm Password */
         $_SESSION['success'] = 'Password Reset Token Generated, Proceed To Confirm Password';
-        $_SESSION['login_username'] = $login_username;
+        $_SESSION['admin_email'] = $admin_email;
         header('Location: confirm_password');
         exit;
     } else {
@@ -55,7 +46,7 @@ if (isset($_POST['Reset_Password_Step_1'])) {
 
 /* Reset Password Step 2 */
 if (isset($_POST['Reset_Password_Step_2'])) {
-    $login_username = mysqli_real_escape_string($mysqli, $_SESSION['login_username']);
+    $admin_email = mysqli_real_escape_string($mysqli, $_SESSION['admin_email']);
     $new_password = sha1(md5(mysqli_real_escape_string($mysqli, $_POST['new_password'])));
     $confirm_password = sha1(md5(mysqli_real_escape_string($mysqli, $_POST['confirm_password'])));
 
@@ -63,7 +54,7 @@ if (isset($_POST['Reset_Password_Step_2'])) {
     if ($new_password != $confirm_password) {
         $err = "Passwords Does Not Match";
     } else {
-        $sql = "UPDATE login SET login_password = '{$confirm_password}' WHERE login_username = '{$login_username}'";
+        $sql = "UPDATE administrator SET admin_password = '{$confirm_password}' WHERE admin_email = '{$admin_email}'";
 
         if (mysqli_query($mysqli, $sql)) {
             /* Pass This Alert Via Session */
