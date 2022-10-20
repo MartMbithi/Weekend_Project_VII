893c8a1c2559eafaf14a93df86c35d9c862aa104
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-20 11:16:17 +0300

    Implement delete admins

--- helpers/admins.php
+++ helpers/admins.php
@@ -18,7 +18,32 @@ if (isset($_POST['Add_Admin'])) {
 }
 /* Update Admin */
 if (isset($_POST['Update_Admin'])) {
+    $admin_id = mysqli_real_escape_string($mysqli, $_POST['admin_id']);
+    $admin_name = mysqli_real_escape_string($mysqli, $_POST['admin_name']);
+    $admin_phone_number = mysqli_real_escape_string($mysqli, $_POST['admin_phone_number']);
+    $admin_email = mysqli_real_escape_string($mysqli, $_POST['admin_email']);
+
+    /* Persist */
+    $sql = "UPDATE admin SET admin_name = '{$admin_name}', admin_phone_number = '{$admin_phone_number}', admin_email = '{$admin_email}'
+    WHERE admin_id = '{$admin_id}'";
+
+    if (mysqli_query($mysqli, $sql)) {
+        $success = "Admin details updated";
+    } else {
+        $err = "Failed, please try again";
+    }
 }
+
 /* Delete Admin */
 if (isset($_POST['Delete_Admin'])) {
+    $admin_id = mysqli_real_escape_string($mysqli, $_POST['admin_id']);
+
+    /* Persist */
+    $sql = "DELETE FROM admin WHERE admin_id = '{$admin_id}'";
+
+    if (mysqli_query($mysqli, $sql)) {
+        $success = "Admin details deleted";
+    } else {
+        $err = "Failed, try again";
+    }
 }
