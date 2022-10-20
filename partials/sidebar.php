da378423073758087b67576e446adc026b09e1dd
Martin Mbithi <martdevelopers254@gmail.com>
2022-10-20 09:15:17 +0300

    Implement Side Toggle Bar

--- partials/sidebar.php
+++ partials/sidebar.php
@@ -18,6 +18,15 @@
                     </a>
                 </li>
             </ul>
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                    <div class="sidenav-toggler-inner">
+                        <i class="sidenav-toggler-line"></i>
+                        <i class="sidenav-toggler-line"></i>
+                        <i class="sidenav-toggler-line"></i>
+                    </div>
+                </a>
+            </li>
         </div>
     </div>
 </nav>
